2621cbf635e1475ad2a0ea9887f37fb9852bba3d
Lain Musgrove <lain.proliant@gmail.com>
2023-08-16 16:37:13 -0700

    Bump to v0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="bivalve",
-    version="0.7",
+    version="0.8",
     description="A bi-directional shell-like socket protocol framework using asyncio.",
     long_description=long_description,
     long_description_content_type="text/markdown",
