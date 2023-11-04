29b96651eacda0a6babbf87d403b726e5758ac06
Lain Musgrove <lain.proliant@gmail.com>
2023-11-04 13:02:32 -0700

    v2.0: Backwards Incompatible - Switch to Binary Line Format

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="bivalve",
-    version="1.2.0",
+    version="2.0.0",
     description="A bi-directional shell-like socket protocol framework using asyncio.",
     long_description=long_description,
     long_description_content_type="text/markdown",
